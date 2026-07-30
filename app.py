d2d6077357200bbffe5c3924c35e3fc0ca9c1d7e
Anishka Shahdev <anishkashshdev9@gmail.com>
2026-07-30 19:39:11 +0530

    Fixed model loading path

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pickle
 
-model = pickle.load(open("model.pkl", "rb"))
+import pickle
+import os
+
+model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
+
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
 
 st.title("Medical Insurance Cost Predictor")
 
